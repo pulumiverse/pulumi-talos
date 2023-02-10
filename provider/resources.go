a5ad98e6e2523979987753532b7eedce35bbf4d7
Ringo De Smet <ringo@de-smet.name>
2023-02-10 13:50:16 +0100

    Publish the JS/TS package in the @siderolabs NPM namespace
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- provider/resources.go
+++ provider/resources.go
@@ -70,6 +70,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		DataSources: map[string]*tfbridge.DataSourceInfo{},
 		JavaScript: &tfbridge.JavaScriptInfo{
+			PackageName: "@siderolabs/pulumi-talos",
 			// List any npm dependencies and their versions
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^3.0.0",
