66a7a7c55b9d9a2189d40faa40c74246230ad5bc
Ringo De Smet <ringo@de-smet.name>
2023-02-10 13:51:04 +0100

    Fix the module path for the Go SDK of this provider
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- provider/resources.go
+++ provider/resources.go
@@ -88,7 +88,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		Golang: &tfbridge.GolangInfo{
 			ImportBasePath: filepath.Join(
-				fmt.Sprintf("github.com/pulumi/pulumi-%[1]s/sdk/", mainPkg),
+				fmt.Sprintf("github.com/siderolabs/pulumi-provider-%[1]s/sdk/", mainPkg),
 				tfbridge.GetModuleMajorVersion(version.Version),
 				"go",
 				mainPkg,
